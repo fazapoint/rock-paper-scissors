353b7db696f06597ea40beb980d3bad8262f562e
Fazarama <53871962+fazapoint@users.noreply.github.com>
2023-10-18 18:43:16 +0700

    modified script.js
    finished getComputerChoice function

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 // create function that randomly return rock, paper or scissors called getComputerChoice
 function getComputerChoice (){
-    console.log('i am getcomputerchoice');
-    choice = a;
-    return choice;
+    let choices = ["Rock", "Paper", "Scissor"];
+    let choice = Math.floor(Math.random() * choices.length)
+    console.log(choices[choice])
+    return choices[choice];
 }
 
 // create function that plays a single round of the game. The function should take two parameters,
 // -playerSelection and computerSelection, make playerSelection case-insensitive (rock, ROCK, RocK)
 function playRound(playerSelection, computerSelection){
-    roundResult = "You Lose! Paper beats Rock"
-    return roundResult;
+    playerSelection = playerSelection.toLowerCase()
+    return playerSelection;
 }
 
 function game(){
@@ -18,7 +19,8 @@ function game(){
 }
 
 //use prompt() to get input from user
-const playerSelection = "rock";
+const playerSelection = "rOcK";
 const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection))
+const modifiedPlayerSelection = playRound(playerSelection, computerSelection);
+console.log(modifiedPlayerSelection)
 
