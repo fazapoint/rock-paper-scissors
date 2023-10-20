6060b4032330f450fc64752b5cb8f33365efe691
Fazarama <53871962+fazapoint@users.noreply.github.com>
2023-10-20 17:57:37 +0700

    modified script.js prototyped playRound() and game()

--- script.js
+++ script.js
@@ -1,26 +1,62 @@
 // create function that randomly return rock, paper or scissors called getComputerChoice
 function getComputerChoice (){
-    let choices = ["Rock", "Paper", "Scissor"];
+    let choices = ["rock", "paper", "scissor"];
     let choice = Math.floor(Math.random() * choices.length)
-    console.log(choices[choice])
     return choices[choice];
 }
 
 // create function that plays a single round of the game. The function should take two parameters,
 // -playerSelection and computerSelection, make playerSelection case-insensitive (rock, ROCK, RocK)
 function playRound(playerSelection, computerSelection){
-    playerSelection = playerSelection.toLowerCase()
-    return playerSelection;
+    // Logic to decide who's the winner
+    if (playerSelection === computerSelection){
+        return "It's a tie round!"
+    } else if (playerSelection === "rock"){
+        //computer = paper, scissor
+        if (computerSelection === "paper"){
+
+        } else{
+
+        }
+    } else if (playerSelection === "paper"){
+        //computer = rock, scissor
+        if (computerSelection === "rock"){
+
+        } else{
+            
+        }
+    } else if (playerSelection === "scissor"){
+        //computer = rock, paper
+        if (computerSelection ==="rock"){
+
+        } else{
+
+        }
+    } else {
+        return "Game logic wrong";
+    }
+  
+    
 }
 
 function game(){
-    // each game is 5 rounds, so use function playRound function 5 times 
-    return '5 rounds each game'
+    // each game is 5 rounds, so use function playRound function 5 times
+    score = [];
+    for(let i=0; i < 5; i++){
+        // use prompt() to get input from user
+        let playerSelection = prompt("Pick rock paper scissor").toLowerCase();
+        // validate input playerSelection
+        if (playerSelection !== "rock" && playerSelection !== "paper" && playerSelection !== "scissor"){
+            let failMsg = "Wrong choice (you need to pick rock, paper or scissor)";
+            return failMsg;
+        }
+    }
+    return '5 rounds each game';
 }
 
-//use prompt() to get input from user
-const playerSelection = "rOcK";
-const computerSelection = getComputerChoice();
-const modifiedPlayerSelection = playRound(playerSelection, computerSelection);
-console.log(modifiedPlayerSelection)
+
+let computerSelection = getComputerChoice();
+const result = playRound(playerSelection, getComputerChoice())
+console.log(result)
+
 
