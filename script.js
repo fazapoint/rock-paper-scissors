f6aac4bea25a46dac46e9b199f9a68ce1bdfcadb
Faza Ramadhani <53871962+fazapoint@users.noreply.github.com>
2023-10-31 22:08:49 +0700

    modified index.html and script.js
    finished playGame() function, deleted getUserChoice() function and created 3 buttons for user input

--- script.js
+++ script.js
@@ -1,21 +1,9 @@
-// create function that randomly return rock, paper or scissors called getComputerChoice
 function getComputerChoice(){
-    let choices = ["rock", "paper", "scissor"];
-    let choice = Math.floor(Math.random() * choices.length)
+    let choices = ["rock", "paper", "scissors"];
+    let choice = Math.floor(Math.random() * choices.length);
     return choices[choice];
 }
 
-function getUserChoice(){
-    // validate input playerSelection, if input wrong then re-prompt, use prompt() to get input from user
-    let choice = prompt("Pick rock paper scissor").toLowerCase();
-    while(choice !== "rock" && choice !== "paper" && choice !== "scissor"){
-        choice = prompt("Invalid choice, please pick rock paper or scissor").toLowerCase();
-    }
-    return choice;
-}
-
-// create function that plays a single round of the game. The function should take two parameters,
-// -playerSelection and computerSelection, make playerSelection case-insensitive (rock, ROCK, RocK)
 function playRound(playerSelection, computerSelection){
     // Logic to decide who's the winner
     if(playerSelection === computerSelection){
@@ -23,9 +11,9 @@ function playRound(playerSelection, computerSelection){
     }
 
     if(
-        (playerSelection === "rock" && computerSelection === "scissor") ||
+        (playerSelection === "rock" && computerSelection === "scissors") ||
         (playerSelection === "paper" && computerSelection === "rock") ||
-        (playerSelection === "scissor" && computerSelection === "paper")
+        (playerSelection === "scissors" && computerSelection === "paper")
     ){
         return `You win, ${playerSelection} beats ${computerSelection}`;
     }else{
@@ -33,32 +21,58 @@ function playRound(playerSelection, computerSelection){
     }
 }
 
-function game(){
-    // each game is 5 rounds, so use function playRound function 5 times
+function playGame(){
+    const buttons = document.querySelectorAll('.btnChoice');
     let playerScore = 0;
     let computerScore = 0;
-    for(let i=0; i < 5; i++){
-        // start the round
-        console.log(`Round ${i+1}`);
-        let playerSelection = getUserChoice();
-        let computerSelection = getComputerChoice();
-        let roundResult = playRound(playerSelection, computerSelection);
-        console.log(roundResult);
-        if(roundResult.includes('You win')){
-            playerScore++;
-        } else if(roundResult.includes('You lost')){
-            computerScore++;
-        }
-    }
-    let endGame;
-    if(playerScore > computerScore){
-        endGame = `Congrats, you win the game! the score is ${playerScore} (you) - ${computerScore} (computer)`;
-    }else if(playerScore < computerScore){
-        endGame = `Sorry, you lost the game! the score is ${playerScore} (you) - ${computerScore} (computer)`;
-    }else{
-        endGame = `It's a tie game! the score is ${playerScore} (you) - ${computerScore} (computer)`;
-    }
-    return endGame;
+
+    buttons.forEach((button) => {
+        button.addEventListener('click', function(){
+            let playerSelection = button.value;
+            let computerSelection = getComputerChoice();
+            let roundResult = playRound(playerSelection, computerSelection);
+            console.log('You chose ' + playerSelection);
+            console.log('Computer chose ' + computerSelection);
+            console.log(roundResult);
+            if (roundResult.includes('You win')){
+                playerScore++;
+            } else if (roundResult.includes('You lost')){
+                computerScore++;
+            }
+            console.log(`Player: ${playerScore} - computer: ${computerScore}`);
+            console.log('--------------------------------------------')
+        });
+    });
 }
 
-console.log(game())
+
+playGame();
+
+
+// function game(){
+//     // each game is 5 rounds, so use function playRound function 5 times
+//     let playerScore = 0;
+//     let computerScore = 0;
+//     for(let i=0; i < 5; i++){
+//         // start the round
+//         console.log(`Round ${i+1}`);
+//         let playerSelection = getUserChoice();
+//         let computerSelection = getComputerChoice();
+//         let roundResult = playRound(playerSelection, computerSelection);
+//         console.log(roundResult);
+//         if(roundResult.includes('You win')){
+//             playerScore++;
+//         } else if(roundResult.includes('You lost')){
+//             computerScore++;
+//         }
+//     }
+//     let endGame;
+//     if(playerScore > computerScore){
+//         endGame = `Congrats, you win the game! the score is ${playerScore} (you) - ${computerScore} (computer)`;
+//     }else if(playerScore < computerScore){
+//         endGame = `Sorry, you lost the game! the score is ${playerScore} (you) - ${computerScore} (computer)`;
+//     }else{
+//         endGame = `It's a tie game! the score is ${playerScore} (you) - ${computerScore} (computer)`;
+//     }
+//     return endGame;
+// }
