ae67aa156f8dcc5406e8e119af910d427c2752fe
Faza Ramadhani <53871962+fazapoint@users.noreply.github.com>
2023-11-02 09:57:09 +0700

    modified scipt.js, index.html, and readme.md
    finished basic function of rock-paper-scissors game

--- README.md
+++ README.md
@@ -4,5 +4,8 @@ You can see the live preview [here](https://fazapoint.github.io/rock-paper-sciss
 
 What i did in this project:
 - pick a random element from an array
-
+- using addEventListener() and use logic for every button clicked
+- using querrySelectorAll() for buttons and use them as an array-like using forEach()
+- button display manipulation for displaying button or make them hidden
+- dom manipulation for game information that being played
 
--- script.js
+++ script.js
@@ -1,78 +1,116 @@
+let playerScore = 0;
+let computerScore = 0;
+const info = document.querySelector('#info');
+const score = document.querySelector('#score');
+const buttons = document.querySelectorAll('.btnChoice');
+const btnPlay = document.querySelector('#btnPlay');
+
 function getComputerChoice(){
     let choices = ["rock", "paper", "scissors"];
     let choice = Math.floor(Math.random() * choices.length);
     return choices[choice];
 }
 
-function playRound(playerSelection, computerSelection){
-    // Logic to decide who's the winner
-    if(playerSelection === computerSelection){
-        return "It's a tie round!";
+function playGame(playerSelection, computerSelection){
+    // remove all child if parent info node still have child (reset every round)
+    while (info.firstChild) {
+        info.removeChild(info.firstChild);
     }
 
-    if(
+    while (score.firstChild) {
+        score.removeChild(score.firstChild);
+    }
+
+    let roundResult = '';
+
+    if(playerSelection === computerSelection){
+        roundResult = "It's a tie round!";
+    } else if(
         (playerSelection === "rock" && computerSelection === "scissors") ||
         (playerSelection === "paper" && computerSelection === "rock") ||
         (playerSelection === "scissors" && computerSelection === "paper")
     ){
-        return `You win, ${playerSelection} beats ${computerSelection}`;
+        roundResult = `You win, ${playerSelection} beats ${computerSelection}`;
     }else{
-        return `You lost, ${computerSelection} beats ${playerSelection}`;
+        roundResult = `You lost, ${computerSelection} beats ${playerSelection}`;
     }
-}
 
-function playGame(){
-    const buttons = document.querySelectorAll('.btnChoice');
-    let playerScore = 0;
-    let computerScore = 0;
+    // append information to the html
+    const playerSelectionInfo = document.createElement('div') 
+    playerSelectionInfo.textContent = 'You chose ' + playerSelection;
+    info.appendChild(playerSelectionInfo);
 
-    buttons.forEach((button) => {
-        button.addEventListener('click', function(){
-            let playerSelection = button.value;
-            let computerSelection = getComputerChoice();
-            let roundResult = playRound(playerSelection, computerSelection);
-            console.log('You chose ' + playerSelection);
-            console.log('Computer chose ' + computerSelection);
-            console.log(roundResult);
-            if (roundResult.includes('You win')){
-                playerScore++;
-            } else if (roundResult.includes('You lost')){
-                computerScore++;
-            }
-            console.log(`Player: ${playerScore} - computer: ${computerScore}`);
-            console.log('--------------------------------------------')
+    const computerSelectionInfo = document.createElement('div') 
+    computerSelectionInfo.textContent = 'Computer chose ' + computerSelection;
+    info.appendChild(computerSelectionInfo);
+
+    const roundResultInfo = document.createElement('div') 
+    roundResultInfo.textContent = roundResult;
+    info.appendChild(roundResultInfo);
+
+    if (roundResult.includes('You win')){
+        playerScore++;
+    } else if (roundResult.includes('You lost')){
+        computerScore++;
+    }
+
+    // append score information to the html
+    const currentScore = document.createElement('div');
+    currentScore.textContent = `Player: ${playerScore} - computer: ${computerScore}`;
+    score.appendChild(currentScore);
+    
+    // if player or computer reached 5 score logic
+    let winner = ''
+    if (playerScore >= 5){
+        winner = 'Congrats! Player win the game!';
+        buttons.forEach(button => {
+            button.style.display = 'none';
+            btnPlay.style.display = 'block';
         });
-    });
+    }else if(computerScore >= 5){
+        winner = 'Computer win the game!';
+        playerScore = 0;
+        computerScore = 0;
+        buttons.forEach(button => {
+            button.style.display = 'none';
+            btnPlay.style.display = 'block';
+        });
+    }
+
+    // append the winner information to the html
+    const winnerInfo = document.createElement('div');
+    winnerInfo.textContent = winner;
+    info.appendChild(winnerInfo)
+
 }
 
+// make choice button hidden
+buttons.forEach(button => {
+    button.style.display = 'none';
+});
 
-playGame();
+// playGame() with argument input of button value
+buttons.forEach(button =>{
+    button.addEventListener('click', function(){
+        playGame(button.value, getComputerChoice())
+    });
+});
 
+// When play button clicked (button appear first time or when winner already decided)
+btnPlay.addEventListener('click', function(){
+    btnPlay.style.display = 'none';
+    buttons.forEach(button => {
+        button.style.display = 'block';
+    });
+    playerScore = 0;
+    computerScore = 0;
 
-// function game(){
-//     // each game is 5 rounds, so use function playRound function 5 times
-//     let playerScore = 0;
-//     let computerScore = 0;
-//     for(let i=0; i < 5; i++){
-//         // start the round
-//         console.log(`Round ${i+1}`);
-//         let playerSelection = getUserChoice();
-//         let computerSelection = getComputerChoice();
-//         let roundResult = playRound(playerSelection, computerSelection);
-//         console.log(roundResult);
-//         if(roundResult.includes('You win')){
-//             playerScore++;
-//         } else if(roundResult.includes('You lost')){
-//             computerScore++;
-//         }
-//     }
-//     let endGame;
-//     if(playerScore > computerScore){
-//         endGame = `Congrats, you win the game! the score is ${playerScore} (you) - ${computerScore} (computer)`;
-//     }else if(playerScore < computerScore){
-//         endGame = `Sorry, you lost the game! the score is ${playerScore} (you) - ${computerScore} (computer)`;
-//     }else{
-//         endGame = `It's a tie game! the score is ${playerScore} (you) - ${computerScore} (computer)`;
-//     }
-//     return endGame;
-// }
+    // remove previous info from the game before
+    while (info.firstChild) {
+        info.removeChild(info.firstChild);
+    }
+
+    while (score.firstChild) {
+        score.removeChild(score.firstChild);
+    }
+})
